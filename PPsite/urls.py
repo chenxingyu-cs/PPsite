77bb55c37e885ea3bc3ab684da022a9e9739ad63
chenxingyu-cs <chenxingyu.cs@gmail.com>
2015-05-09 12:38:48 +0800

    add login and related pages

--- PPsite/urls.py
+++ PPsite/urls.py
@@ -3,10 +3,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
 urlpatterns = [
     # Examples:
-    url(r'^$', 'PPsite.views.home', name='home'),
+    url(r'^$', views.home, name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^signin/', views.sign_in, name='sign_in'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
--- PPsite/views.py
+++ PPsite/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
 
@@ -6,3 +8,23 @@ def home(request):
     context = {}
     template = 'home.html'
     return render(request, template, context)
+
+def sign_in(request):
+    if request.POST:
+        userName = request.POST['userName']
+        password = request.POST['password']
+        user = authenticate(username=userName, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                # return HttpResponseRedirect('/disk/')
+                return HttpResponse('Sign in success!')
+            else:
+                #return HttpResponseRedirect('/login/')
+                return HttpResponseRedirect('')
+    return render(request, 'signin.html')
+
+def sign_out(request):
+    logout(request)
+    return render('home.html')
+
